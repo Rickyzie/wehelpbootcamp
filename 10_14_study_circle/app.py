a551c1c0fd62ac17f775e3239ab0460cd3f56fc8
Ricky <a0935640996@gmail.com>
2022-10-13 15:32:15 +0800

    n

--- 10_14_study_circle/app.py
+++ 10_14_study_circle/app.py
@@ -2,10 +2,31 @@ from flask import Flask
 from flask import request
 from flask import Response
 from markupsafe import escape
-from flask.ext.cors import CORS, cross_origin
+from flask_cors import CORS
+from flask import Flask, session
+from flask_session.__init__ import Session
 
 app = Flask(__name__)
+# Check Configuration section for more details
 
+#eyJrZXkiOiJhc2Rkc2EifQ.Y0e7xg.dTIwDDYRjTlkNHE9dZQLKnIYWag
+#eyJrZXkiOiJhc2Rkc2FhYWFhYSJ9.Y0e79A.gLzuAuv0vMihbDR3PYl_6DAchb0
+#eyJrZXkiOiIifQ.Y0e8BA.X_fUj4GSTUuTUWaBN3PQP1ivaEY
+#eyJrZXkiOm51bGx9.Y0e8LA.5fw5HRcKzQDxS5DSTHA3tHfl7Lo
+cors = CORS(app, resources={r"/fetch": {"origins": "*"}})
+
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+app.secret_key =  "1"
+
+@app.route('/set/')
+def set():
+    session['key'] = request.args.get('data')
+    return 'ok'
+
+@app.route('/get/')
+def get():
+    return session.get('key', 'not set')
 
 @app.route('/form', methods=['POST'])
 def form():
@@ -29,8 +50,7 @@ def json():
 cors = CORS(app, resources={r"/fetch": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/fetch', methods=['POST','OPTIONS'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@app.route('/fetch', methods=['POST'])
 def fetch():
     print(request.content_type)
     print(request.json)
